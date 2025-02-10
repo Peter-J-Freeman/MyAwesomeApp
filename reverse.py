4d08c3b5d2022a91ec0228b6db837e0c7a06c9e0
Maryam <155160218+M-YP@users.noreply.github.com>
2025-02-10 18:46:31 +0000

    defined the function that reverses the strand sequence

--- reverse.py
+++ reverse.py
@@ -1,4 +1,7 @@
-# reverse.py 
+def reverse_string_slicing(strand):
+    print(f"call reverce_string_slicing({strand})")
+    return strand[::-1]
+
 strand = "atgcatgc"
 generate = reverse_string_slicing(strand)
 print(generate)
